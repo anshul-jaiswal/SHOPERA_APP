fa322f30073c705f8658951ff01e99d459b5528d
anshul jaiswal <anshuljaiswal568@gmail.com>
2025-07-04 18:39:00 +0530

    url changed

--- ui/src/Apiurl.js
+++ ui/src/Apiurl.js
@@ -1,7 +1,7 @@
-export const __userApi="http://localhost:3001/user/";
-export const __categoryApiurl="http://localhost:3001/category/"
+export const __userApi="https://shopera-app.onrender.com/user/";
+export const __categoryApiurl="https://shopera-app.onrender.com/category/"
 
-export const __subcategoryapiurl="http://localhost:3001/subcategory/";
-export const __Productapiurl="http://localhost:3001/product/";
-export const __paymentapiUrl="http://localhost:3001/payment/";
-export const __RozarpayURL="http://localhost:3001/"
+export const __subcategoryapiurl="https://shopera-app.onrender.com/subcategory/";
+export const __Productapiurl="https://shopera-app.onrender.com/product/";
+export const __paymentapiUrl="https://shopera-app.onrender.com/payment/";
+export const __RozarpayURL="https://shopera-app.onrender.com/"
