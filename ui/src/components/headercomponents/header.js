669a7f21e7ecae4fb81d1d3ed28ed80d345983bd
anshul jaiswal <anshuljaiswal568@gmail.com>
2025-07-06 19:03:47 +0530

    some error in text

--- ui/src/components/headercomponents/header.js
+++ ui/src/components/headercomponents/header.js
@@ -17,7 +17,7 @@ function Header() {
                             <div class="row gx-0">
                                 <div class="col-lg-3 bg-dark d-none d-lg-block">
                                     <a class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                                        <h1 class="m-0 text-primary text-uppercase">Shopera hello</h1>
+                                        <h1 class="m-0 text-primary text-uppercase">Shopera</h1>
                                     </a>
                                 </div>
                                 <div class="col-lg-9">
